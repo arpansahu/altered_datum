2453cba8f8e2fbefea616f86e02e5e7633c4f2c2
Arpan Sahu <arpanrocks95@gmail.com>
2024-09-01 13:33:24 +0530

    added node modules

---
+++ build/precache-manifest.0a59fb1500ba0c9515172deb5e6c4586.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107b0aaa112246f51eca9d126bcd5ff1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3222f48cbc1da5c4dbda",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "6e606d88a91b2c7c71c8",
+    "url": "/static/js/2.fc85856a.chunk.js"
+  },
+  {
+    "revision": "1a042000e9301921a4141483110c357d",
+    "url": "/static/js/2.fc85856a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3222f48cbc1da5c4dbda",
+    "url": "/static/js/main.9022ec70.chunk.js"
+  },
+  {
+    "revision": "ad76c1e59cbba06fc5e5",
+    "url": "/static/js/runtime-main.82fb1a33.js"
+  },
+  {
+    "revision": "1bcb8bb46f1620513efe95f2c3ea0331",
+    "url": "/static/media/logo.1bcb8bb4.png"
+  }
+]);
